adc18185ac7d19eb77a755231d3b2c509460e1ee
simplemart01 <simplemart01@gmail.com>
2016-10-15 12:50:32 -0700

    Array support

--- simplemart-javason/src/main/java/org/simplemart/javason/Javason.java
+++ simplemart-javason/src/main/java/org/simplemart/javason/Javason.java
@@ -1,5 +1,6 @@
 package org.simplemart.javason;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -9,16 +10,35 @@ public class Javason {
 
     private final Map<String, Object> attributes = new HashMap<>();
 
+    private boolean formatted;
+
+    public void setFormatted(boolean formatted) {
+        this.formatted = formatted;
+    }
+
     public Javason o(String name, Object value) {
         require(name, "name");
-        attributes.put(name, value);
+        if (value != null && value.getClass().isArray()) {
+            o(name, (Object[]) value);
+        } else {
+            attributes.put(name, value);
+        }
+        return this;
+    }
+
+    public Javason o(String name, Object[] array) {
+        require(name, "name");
+        attributes.put(name, Arrays.asList(array));
         return this;
     }
 
     @Override
     public String toString() {
         StringBuilder jsonString = new StringBuilder("{");
-        String s = attributes.entrySet().stream().map(Javason::toString).collect(Collectors.joining(", "));
+        if (formatted) {
+            jsonString.append("\n");
+        }
+        String s = attributes.entrySet().stream().map(Javason::toString).collect(Collectors.joining(",\n"));
         jsonString.append(s);
         return jsonString.append("}").toString();
     }
@@ -34,13 +54,18 @@ public class Javason {
     }
 
     private static String toString(Object v) {
-        if (v instanceof String) {
+        if (v == null) {
+            return "null";
+        } else if (v instanceof String) {
             return '"' + v.toString() + '"';
         }
         return v.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(new Javason().o("name", "Javason").toString());
+        System.out.println(new Javason()
+                .o("array", "a")
+                .o("a", "a")
+                .toString());
     }
 }
--- simplemart-javason/src/test/java/org/simplemart/javason/JavasonTest.java
+++ simplemart-javason/src/test/java/org/simplemart/javason/JavasonTest.java
@@ -11,7 +11,13 @@ public class JavasonTest {
     public Object[][] getAttributes() {
         return new Object[][] {
                 {"string", "foo", json("{'string': 'foo'}")},
-                {"number", 10, json("{'number': 10}")},
+                {"integer", 10, json("{'integer': 10}")},
+                {"double", 10.1, json("{'double': 10.1}")},
+                {"boolean", true, json("{'boolean': true}")},
+                {"boolean", false, json("{'boolean': false}")},
+                {"empty", null, json("{'empty': null}")},
+                {"array", new Integer[] {3, 4}, json("{'array': [3, 4]}")},
+                {"object", j().o("nested", "a"), json("{'object': {'nested': 'a'}}")},
         };
     }
 
